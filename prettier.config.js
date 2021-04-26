81c526ab1c14c9c5c60f90f896f75671c40a59a4
fonice <fonice@live.cn>
2021-04-27 01:00:56 +0800

    初始化

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  vueIndentScriptAndStyle: true, // vue文件顶层要有缩进
+  singleQuote: true,
+  quoteProps: 'as-needed', // 引用对象中的属性时更改-仅在需要时在对象属性周围添加引号
+  bracketSpacing: true, // 在对象文字中打印括号之间的空格。
+  trailingComma: 'none', // 多行时尽可能打印尾随逗号。
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  arrowParens: 'always', // 箭头函数圆括号-在单个箭头函数参数周围加上括号。
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+  rangeStart: 0
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script lang="tsx">
+  import { ref, defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'HelloWorld',
+    props: {
+      msg: {
+        type: String,
+        required: true
+      }
+    },
+    setup: (props) => {
+      const count = ref(0)
+      return () => (
+        <>
+          <h1>{props.msg}</h1>
+          <br />
+          <button
+            onClick={() => {
+              count.value++
+            }}
+          >
+            count is: {count.value}
+          </button>
+        </>
+      )
+    }
+  })
+</script>
+
+<style scoped>
+  a {
+    color: #42b983;
+  }
+
+  label {
+    margin: 0 0.5em;
+    font-weight: bold;
+  }
+
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+  }
+</style>
